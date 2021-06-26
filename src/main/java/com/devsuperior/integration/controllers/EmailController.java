a159fb977f252af97019f3ecb1b69fda14f756b0
nabucodonosor-java <nabucodonosor.java@gmail.com>
2021-06-26 10:13:43 -0300

    Sending first email

--- src/main/java/com/devsuperior/integration/controllers/EmailController.java
+++ src/main/java/com/devsuperior/integration/controllers/EmailController.java
@@ -1,5 +1,6 @@
 package com.devsuperior.integration.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,13 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.integration.dto.EmailDto;
+import com.devsuperior.integration.services.EmailService;
 
 @RestController
 @RequestMapping(value = "/emails")
 public class EmailController {
 	
+	@Autowired
+	private EmailService service;
+	
 	@PostMapping
 	public ResponseEntity<Void> send(@RequestBody EmailDto dto) {
+		service.sendEmail(dto);
 		return ResponseEntity.noContent().build();
 	}
 
